403a761bc9279e92ef31ca7e5fa375d1223a781c
Ashok Koda <a.koda59@gmail.com>
2021-12-12 15:46:47 +0530

    UC1:Firstname is started with upperCase

--- src/main/java/bridelabzuserregisttration/UserRegistration.java
+++ src/main/java/bridelabzuserregisttration/UserRegistration.java
@@ -1,11 +1,26 @@
 package bridelabzuserregisttration;
 
+import java.util.Scanner;
+
 public class UserRegistration {
+	
+	public static void firstName(String fName) {
+		if(fName.matches("[A-Z][a-z]*")) {
+			System.out.println("First name is valid.....!");
+		}else {
+			System.out.println("First name is not  valid.....!");
+		}
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("-----------Welcome to User Registration------------");
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String fname = sc.next();
+		System.out.println("First Name = " + fname);
+		firstName(fname);
 	}
 
 }
