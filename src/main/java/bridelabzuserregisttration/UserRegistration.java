02819470f8b39271bb099d2bfa6d6dd6b0cf7fc4
Ashok Koda <a.koda59@gmail.com>
2021-12-12 14:05:01 +0530

    Welcome to User registration

---
+++ src/main/java/bridelabzuserregisttration/UserRegistration.java
@@ -0,0 +1,11 @@
+package bridelabzuserregisttration;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		
+		System.out.println("-----------Welcome to User Registration------------");
+
+	}
+
+}
